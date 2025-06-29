46d6bad0869834420f9a679660abb9acddd3fcfb
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-06-29 14:51:14 +0200

    fixing prettier errors

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 A simple app to manage personal tasks, meetings, and action items
 
-
 ##Features
 -Add people  
-*Add meetings
-  -  Add action items and assign them
- -mark them done  
-- light/dark mode toggle 
+\*Add meetings
+
+- Add action items and assign them
+  -mark them done
+- light/dark mode toggle
 
 ##Usage
 
@@ -17,7 +17,6 @@ A simple app to manage personal tasks, meetings, and action items
 2. Run the app (no build step needed)
 
 3.View and manage todos in browser
-
 
 ##License
 MIT
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-const securityPlugin = await import("eslint-plugin-security").then(mod => mod.default || mod);
-const prettierConfig = await import("eslint-config-prettier").then(mod => mod.default || mod);
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+const securityPlugin = await import('eslint-plugin-security').then(
+  (mod) => mod.default || mod,
+);
+const prettierConfig = await import('eslint-config-prettier').then(
+  (mod) => mod.default || mod,
+);
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.browser },
     plugins: {
       js,
@@ -15,15 +18,15 @@ export default defineConfig([
     },
     rules: {
       ...js.configs.recommended.rules, // brings in the recommended rules
-      curly: ["error", "all"],
-      eqeqeq: ["error", "always"],
-      "no-var": "error", 
-      "prefer-const": "error",
-      "prefer-arrow-callback": "error",
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      "security/detect-eval-with-expression": "error",
+      curly: ['error', 'all'],
+      eqeqeq: ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      'security/detect-eval-with-expression': 'error',
     },
   },
-  prettierConfig
+  prettierConfig,
 ]);
